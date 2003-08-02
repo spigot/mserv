b9cd9d87728dafc18070d587902a557798e05ec7
squish <squish>
2003-08-02 14:26:19 +0000

    update copyright

--- mserv/misc.c
+++ mserv/misc.c
@@ -2,7 +2,7 @@
 All of the documentation and software included in the Mserv releases is
 copyrighted by James Ponder <james@squish.net>.
 
-Copyright 1999, 2000 James Ponder.  All rights reserved.
+Copyright 1999-2003 James Ponder.  All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
